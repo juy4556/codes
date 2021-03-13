057e4f85cc6778e897c8e49ce7887e4e5a744260
JunSeok <kws05050@naver.com>
2021-03-13 20:04:51 +0900

    백준1936

---
+++ .vscode/백준1936(재미있는 수학 문제)/백준1936.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+unsigned long long n;
+int p;
+int count = 1;//f(n,0)=1
+scanf_s("%lld %d",&n,&p);
+//f(n,1)=n(n+2)/2
+if(p==2)count++;
+else if(p==n || p==n+1)count++;
+//f(n,2)=n(n-1)(n+1)(3n+2)/24
+count++;
+if(p==n || p==n-1 || p==n+1 || p==3n+2)count--;
+if(p>n)count++;//f(n,n)=n!
+
+
+
+    return 0;
+}
