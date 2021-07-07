1bddf6ec0eaa7bb60069e06e5f321ce271588fec
JunSeok <kws05050@naver.com>
2021-07-07 16:24:48 +0900

    enter

--- .vscode/백준1936(재미있는 수학 문제)/백준1936.cpp
+++ .vscode/백준1936(재미있는 수학 문제)/백준1936.cpp
@@ -1,18 +1,26 @@
+/*백준1936(재미있는 수학 문제)
+f(n,k)가 p로 나누어 떨어지지 않는 k(0<=k<=n)의 수 구하기
+첫째 줄 정수 n과 소수 p 주어짐
+문제 조건을 만족하는 k의 개수를 (10^9+7)로 나눈 나머지 출력*/
 #include <iostream>
 using namespace std;
 
 int main(){
 unsigned long long n;
 int p;
-int count = 1;//f(n,0)=1
 scanf_s("%lld %d",&n,&p);
+int count = n;
 //f(n,1)=n(n+2)/2
-if(p==2)count++;
-else if(p==n || p==n+1)count++;
+if(n==2&&p==2)count--;
+else if(p==n || p==n+1)count--;
 //f(n,2)=n(n-1)(n+1)(3n+2)/24
-count++;
-if(p==n || p==n-1 || p==n+1 || p==3n+2)count--;
-if(p>n)count++;//f(n,n)=n!
+if(p==n || p==n-1 || p==n+1 || p==(3*n+2))count--;
+//f(n,3)
+
+//f(n,n-1)=(n-1)!*...
+if(p<n)count--;
+//f(n,n)=n!
+if(p<=n)count--;
 
     return 0;
 }
---
+++ 백준 1115(순열)/백준 1115.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node{
+    private:
+    int key;
+    Node* link;
+    public:
+    Node(int n,Node* next){
+        key=n;link=next;
+    }
+};
+class LinkedList{
+    friend class Node;
+    private:
+    Node *first;
+    public:
+    LinkedList(){first=0;}
+
+};
+
+int main(){
+    int n;
+    vector<int> v(1,0);
+    scanf_s("%d",&n);
+    int buf;
+    for(int i=0;i<n;i++){
+        scanf_s("%d",&buf);
+        v.push_back(buf);
+    }
+    for(int i=0;i<n;i++){
+        printf("%d ",v[i]);
+    }
+    return 0;
+}
