af7a60bc0f920556b8a1ffb13e3fa3d02db4624c
juy4556 <70626308+juy4556@users.noreply.github.com>
2021-11-11 22:10:13 +0900

    add 5904(moo)

--- Project4/소스.cpp
+++ Project4/소스.cpp
@@ -1,22 +1,34 @@
-#include <stdio.h>
-
+//5904 Moo 게임
+#include <iostream>
+using namespace std;
+int N;//1<=N<=10^9
+int mid = 3;//가운데길이
+int s = 3;//전체길이
 int main() {
-	int num = -1;
-	int x;//정수형 변수 선언
-	scanf_s("%d", &x);//x입력
-	
-	switch (x) {
-	case -1:
-		num--;
-		break;
-	case 1:
-		num--;
-		break;
-	default:
-		num = 0;
-		break;
-	}
-	printf("x:%d\n", x);
-	printf("num:%d", num);
-	return 0;
+    int len = 0;
+    scanf_s("%d", &N);
+    while (N > s) {
+        mid++;
+        s = s * 2 + mid;
+    }
+    while (1) {
+        int len = (s - mid) / 2;
+
+        if (N <= len) {//N앞부분일때
+            mid--;
+            s = len;
+        }
+        else if (N > len + mid) {//N뒷부분일때
+            N -= len + mid;
+            mid--;
+            s = len;
+        }
+        else {//N이 가운데부분일때
+            N -= len;
+            break;
+        }
+    }
+    if (N == 1)cout << "m";//가운데 부분에서 첫번째(m) 제외하고 모두 (o)
+    else cout << "o";
+    return 0;
 }
---
+++ algorithm study/DivideAndConquer/5904.cpp
@@ -0,0 +1,34 @@
+//5904 Moo 게임
+#include <iostream>
+using namespace std;
+int N;//1<=N<=10^9
+int mid=3;//가운데길이
+int s=3;//전체길이
+int main(){
+    int len=0;
+    scanf("%d",&N);
+    while(N>s){
+        mid++;
+        s=s*2+mid;
+    }
+    while(1){
+        int len=(s-mid)/2;
+
+        if(N<=len){//N앞부분일때
+            mid--;
+            s=len;
+        }
+        else if(N>len+mid){//N뒷부분일때
+            N-=len+mid;
+            mid--;
+            s=len;
+        }
+        else{//N이 가운데부분일때
+            N-=len;
+            break;
+        }
+    }
+    if(N==1)cout<<"m";//가운데 부분에서 첫번째(m) 제외하고 모두 (o)
+    else cout<<"o";
+    return 0;
+}
